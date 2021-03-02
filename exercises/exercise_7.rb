201ca8a9e01af0ab2051bae414629607021117bd
Sabia Tahir <sabiatahir@hotmail.com>
2021-03-02 03:10:10 +0000

    Validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Enter a store name"
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+
+# new_employee = @store2.employees.create(first_name: "Max", last_name: "Bill", hourly_rate: 40)
+# new_store.errors.messages.each { |error| puts error }
+puts new_store.errors.messages
